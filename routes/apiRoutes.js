8158d7d08a19c83f6a9b0a71612fb587aef1f0dc
Liv <lopez.olivia17@outlook.com>
2021-04-22 15:56:28 -0500

    adjuste apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,40 @@
-const { json } = require('express');
+const {
+  json,
+  text
+} = require('express');
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const {
+  v4: uuidv4
+} = require('uuid');
 
+// notes = {}
 
 module.exports = (app) => {
 
-  app.get('/api/notes', (req, res) => {
-    // read from the json file 
-    fs.readFile('./db/db.json', (err, data) => {
-    if (err) throw err;
-    // parse it 
-    let notes = JSON.parse(data);
-    // respond back with it 
-    res.json(notes);
+    app.get('/api/notes', (req, res) => {
+      // read from the json file 
+      fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        // parse it 
+        let notes = JSON.parse(data);
+        // respond back with it 
+        res.json(notes);
+      });
     });
-  });
 
-  app.post('/api/notes', (req, res) => {
-    // take in user input from the req body
-    const newNote = {
-      title: req.body.title,
-      text: req.body.text,
-      // uuid
-      id: uuidv4()
-    }.then(() => res.json)
-    // write it to the file (fs.writeFile)
-    // res.json(theCreateThing) 
-  });
-
-  // writeToFile('db/db.json', newNote())
-
-// function to write html file 
-// function writeToFile(fileName, data) {
-//   fs.writeFile(fileName, data, (error) => {
-//     if(error) throw error;
-//     console.log(data)
-//   });
-// };
-
-
-
-
-  // app.delete('/api/notes/:noteId', () => {
-  //   // noteId
-  //   // req
-  // })
+    app.post('/api/notes', (req, res) => {
+          // take in user input from the req body
+          const newNote = req.body;
+          newNote.id = uuidv4();
+          fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            const notes = JSON.parse(data);
+            notes.push(newNote)
+          });
+          // write it to the file (fs.writeFile)
+          fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(newNote)
+          });
+        })
 };
